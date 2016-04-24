7cb946e709a57bb01ec8dab7ac503e1ec8996915
Deanna Arora <aroradeanna2@gmail.com>
2016-04-24 23:31:13 +1000

    Fixed all syntax errors. Still has other errors.

--- sampletext.sql
+++ sampletext.sql
@@ -1,6 +1,6 @@
 INSERT INTO Location (name, type)
 VALUES
-	('Curtin District', 'Magically Hidden')
+	('Curtin District', 'Magically Hidden'),
 	('Crace District', 'Parking Lot'),
 	('Yarralumla District', 'Portal Allocation'),
 	('Deakin District', 'Singular Spaces'),
@@ -24,14 +24,27 @@ VALUES
 	('Silver', '$20', '$40', '$3', '$85', '$8', '$50'),
 	('Bronze', '$10', '$50', '$4.5', '$95', '$8.50', '$35');
 
-INSERT INTO Member
-VALUES('lightingbolt@gmail.com', 'Mr', 'Harry', 'Potter', 'Scarhead' 'Alohomora', 394, '8/9/2016', '13 Digby CCT, Crace, ACT, Australia', 'Leaky Cauldron', '31 July 1980', 'Gold', '31 July 1993');
 
+BEGIN;
+INSERT INTO Member
+VALUES('lightingbolt@gmail.com', 'Mr', 'Harry', 'Potter', 'Scarhead', 'Alohomora', 394, '8/9/2016', '13 Digby CCT, Crace, ACT, Australia', 'Leaky Cauldron', '31 July 1980', 'Gold', '31 July 1993');
+INSERT INTO PaymentMethod (email)
+VALUES ('lightingbolt@gmail.com');
+COMMIT;
+
+BEGIN;
 INSERT INTO Member (email, title, family_name, given_name, nickname, password, license, license_expiry, address, fav_bay_name, birthdate, membership_plan, member_since)
 VALUES
 	('twinkle@hotmail.com', 'Mx', 'Albus', 'Dumbledore', 'Dumbly-dorr', 'I open at the close', 456, '4 July 2016', '5A Dawson Street, Yarralumla, ACT, Australia', 'Hogwarts', '1 Feb 1950', 'Platinum', '2 Dec 1960'),
-	('muggleborn@outlook.com', 'Mrs', 'Hermione', 'Granger-Malfoy', 'Bookworm', 'Dramione', 444, '17/12/2017', '2 Philip Place, Curtin, ACT', 'Three Broomsticks', '19 Sept 1980', 'Silver', '2 Dec 1993'),
-	('dragon@iprimus.com', 'Mr', 'Draco', 'Malfoy', 'Ferret', 'Redeemed', 888, '24/10/2017', '2 Philip Place, Curtin, ACT', 'Hogsmeade', '5 June 1980', 'Platinum', '2 Dec 1993');
+	('muggleborn@outlook.com', 'Mrs', 'Hermione', 'Granger-Malfoy', 'Bookworm', 'Dramione', 444, '12/17/2016', '2 Philip Place, Curtin, ACT', 'Three Broomsticks', '19 Sept 1980', 'Silver', '2 Dec 1993'),
+	('dragon@iprimus.com', 'Mr', 'Draco', 'Malfoy', 'Ferret', 'Redeemed', 888, '10/2/2016', '2 Philip Place, Curtin, ACT', 'Hogsmeade', '5 June 1980', 'Platinum', '2 Dec 1993');
+INSERT INTO PaymentMethod (email)
+VALUES
+	('muggleborn@outlook.com'),
+	('twinkle@hotmail.com'),
+	('dragon@iprimus.com');
+COMMIT;
+
 
 INSERT INTO Phone (email, phone)
 VALUES
@@ -49,7 +62,7 @@ VALUES
 INSERT INTO CarModel (make, model, capacity, category)
 VALUES
 	('Toyota', 'Camry', 5, 'Sedan'),
-	('Nissan', 'Dualis', 5 'SUV'),
+	('Nissan', 'Dualis', 5, 'SUV'),
 	('Nimbus', '2000', 2, 'Racing Broomstick'),
 	('Nimbus', 'Firebolt', 2, 'Racing Broomstick');
 
@@ -65,11 +78,5 @@ VALUES
 INSERT INTO Booking (regno, startDate, startHour, duration, whenBooked, bookedBy)
 VALUES
 	('YBK9OC', '2/08/2016', 15, 4, CURRENT_TIMESTAMP, 'muggleborn@outlook.com'),
-	('2TOO22', '13/07/2016', 9, 12, CURRENT_TIMESTAMP, 'twinkle@hotmail.com'),
+	('2TOO22', '12/07/2016', 9, 12, CURRENT_TIMESTAMP, 'twinkle@hotmail.com'),
 	('4FOUR4', '8/12/2016', 18, 23, CURRENT_TIMESTAMP, 'scarhead@gmail.com');
-
-INSERT INTO PaymentMethod (email)
-VALUES
-	('muggleborn@outlook.com'),
-	('twinkle@hotmail.com'),
-	('lightingbolt@gmail.com');
