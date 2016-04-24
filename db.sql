6669b77ebb90ada9aac2243a9adfe6fca8006533
Cai-Colin <ccai1997@hotmail.com>
2016-04-24 11:35:18 +1000

    Attempt at fixing syntax
    Now this will actually run in PostgreSQL.
    Needs to be checked though to make sure the changes are okay. It now
    successfully runs in postgresql

--- db.sql
+++ db.sql
@@ -1,5 +1,31 @@
+CREATE TABLE Location (
+	id integer PRIMARY KEY,
+	name varchar(50),
+	type varchar(50),
+	partOf integer references Location(id)
+);
+
+CREATE TABLE CarBay (
+	name varchar(50) PRIMARY KEY,
+	address text,
+	description text,
+	location integer references Location(id) NOT NULL,
+	latitude real,
+	longitude real
+);
+
+CREATE TABLE MembershipPlan (
+	name varchar(50) PRIMARY KEY,
+	monthly_rate money,
+	hourly_rate money,
+	km_rate money,
+	daily_rate money,
+	daily_km_rate money,
+	daily_km_rate_included money
+);
+
 CREATE TABLE Member (
-	email varchar(50) primary key,
+	email varchar(50) PRIMARY KEY,
 	title varchar(4),
 	family_name varchar(50),
 	given_name varchar(50),
@@ -10,23 +36,13 @@ CREATE TABLE Member (
  	address varchar(50),
  	fav_bay_name varchar(50) references CarBay(name),
  	birthdate date,
- 	membership_plan varchar(50) references MembershipPlan(title) NOT NULL, 
- 	member_since date, 
- 	preferred_payment integer references PaymentMethod(paymentNum) NOT NULL
+ 	membership_plan varchar(50) references MembershipPlan(name) NOT NULL, 
+ 	member_since date
 );
 
 CREATE TABLE Phone (
 	email varchar(50) references Member(email) NOT NULL,
 	phone varchar(10) -- Maximum standard *Australian* mobile phone number
-);
-
-CREATE TABLE Car (
-	regno char(6) primary key,
-	make varchar(30) references CarModel(make) NOT NULL,
-	model varchar(30) references CarModel(model) NOT NULL,
-	year integer,
-	transmission varchar(50),
-	parkedAt integer references CarBay(name) NOT NULL
 );
 
 CREATE TABLE CarModel (
@@ -35,7 +51,17 @@ CREATE TABLE CarModel (
 	name varchar(50),
 	capacity integer,
 	category varchar(50),
-	primary key (make, model)
+	PRIMARY KEY (make, model)
+);
+
+CREATE TABLE Car (
+	regno char(6) PRIMARY KEY,
+	make varchar(30) NOT NULL,
+	model varchar(30) NOT NULL,
+	FOREIGN KEY (make, model) references CarModel(make, model),
+	year integer,
+	transmission varchar(50),
+	parkedAt varchar(50) references CarBay(name) NOT NULL
 );
 
 CREATE TABLE Booking (
@@ -47,48 +73,23 @@ CREATE TABLE Booking (
 	duration integer, -- limiting to hourly bookings
 	whenBooked timestamp, 
 	bookedBy varchar(50) references Member(email) NOT NULL,
-	primary key (regno, startDate, startHour)
-);
-
-CREATE TABLE CarBay (
-	name varchar(50) primary key,
-	address text,
-	description text,
-	location integer references Location(id) NOT NULL,
-	latitude real,
-	longitude real
-);
-
-CREATE TABLE Location (
-	id integer primary key,
-	name varchar(50),
-	type varchar(50),
-	partOf integer references Location(id)
-);
-
-CREATE TABLE MembershipPlan (
-	name varchar(50) primary key,
-	monthly_rate money,
-	hourly_rate money,
-	km_rate money,
-	daily_rate money,
-	daily_km_rate money,
-	daily_km_rate_included money
+	PRIMARY KEY (regno, startDate, startHour)
 );
 
 CREATE TABLE PaymentMethod (
-	payment_num integer,
-	email varchar(50) references Member(preferred_payment),
-	primary key(payment_num, email)
+	paymentNum integer PRIMARY KEY,
+	email varchar(50) references Member(email)
 );
 
+ALTER TABLE Member ADD COLUMN preferred_payment integer references PaymentMethod(paymentNum) NOT NULL;
+
 CREATE TABLE Paypal (
-	payment_num integer primary key references PaymentMethod(payment_num),
+	paymentNum integer PRIMARY KEY references PaymentMethod(paymentNum),
 	paypal_email varchar(50)
 );
 
 CREATE TABLE BankAccount (
-	payment_num integer primary key references PaymentMethod(payment_num),
+	paymentNum integer PRIMARY KEY references PaymentMethod(paymentNum),
 	name varchar(30),
 	-- constraint of 3 digits space 3 digits for bsb --
 	bsb char(7),
@@ -96,7 +97,7 @@ CREATE TABLE BankAccount (
 );
 
 CREATE TABLE CreditCard (
-	payment_num integer primary key references PaymentMethod(payment_num),
+	paymentNum integer PRIMARY KEY references PaymentMethod(paymentNum),
 	name varchar(30),
 	brand varchar(30),
 	expires date,
